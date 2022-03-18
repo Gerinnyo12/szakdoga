d0daf5d0bca27534c9ef2a7856f57401349dff03
reveszg <reveszg@REVESZG-W10E>
2022-03-18 13:58:16 +0100

    Tests

---
+++ Tests/NoDefaultCtor/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Interfaces;
+
+namespace NoDefaultCtor
+{
+    public class Class1 : IWorkerTask
+    {
+        public uint Timer => 3;
+
+        public async Task Run()
+        {
+            
+        }
+
+        public Class1(string asd, int qwe)
+        {
+
+        }
+    }
+}
---
+++ Tests/SlowConstructor/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Interfaces;
+
+namespace SlowConstructor
+{
+    public class Class1 : IWorkerTask
+    {
+        public uint Timer => 5;
+
+        public async Task Run()
+        {
+            
+        }
+
+        public Class1()
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
